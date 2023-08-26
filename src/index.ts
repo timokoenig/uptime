940fa1fa2234a58416e3e6005c12aebb448d4643
Timo Koenig <timo@timokoenig.com>
2023-08-27 01:08:01 +0200

    Add mustache template engine

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as http from "http";
 import * as cron from "node-cron";
+import * as fs from "fs";
+import * as path from "path";
+import * as mustache from "mustache";
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -9,8 +12,18 @@ let cronValue = 0;
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(`CronValue: ${cronValue}`);
+    res.setHeader("Content-Type", "text/html");
+
+    const html = mustache.render(
+      fs
+        .readFileSync(path.join(__dirname, "../templates/index.mustache"))
+        .toString(),
+      {
+        count: cronValue,
+      }
+    );
+
+    res.end(html);
   }
 );
 
