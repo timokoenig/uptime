0ab41b06cf6fb1127c6c18edde86fafc02176d72
Timo Koenig <timo@timokoenig.com>
2023-08-27 10:28:06 +0200

    Ping services one after another to avoid json write conflicts

--- src/handler/cron.ts
+++ src/handler/cron.ts
@@ -5,34 +5,34 @@ import { readConfig } from '../utils/helper'
 export const cronHandler = async () => {
   try {
     const config = readConfig()
-    await Promise.all(
-      config.services.map(async service => {
-        const date = moment()
-        console.log(`${date.toISOString()} | ➡️  Ping ${service.url}`)
 
-        const response = await fetch(service.url)
-        const latencyMs = moment().diff(date, 'milliseconds')
+    // ping services one after another to avoid json write conflicts
+    for (const service of config.services) {
+      const date = moment()
+      console.log(`${date.toISOString()} | ➡️  Ping ${service.url}`)
 
-        if (response.ok) {
-          console.log(
-            `${moment().toISOString()} | ⬅️  Pong ${service.url} | ${latencyMs}ms | Status ${
-              response.status
-            }`
-          )
-        } else {
-          console.log(
-            `${moment().toISOString()} | ⬅️  Pong failed ${service.url} | Status ${response.status}`
-          )
-        }
+      const response = await fetch(service.url)
+      const latencyMs = moment().diff(date, 'milliseconds')
 
-        writePing({
-          date,
-          latency: latencyMs,
-          status: response.status,
-          url: service.url,
-        })
+      if (response.ok) {
+        console.log(
+          `${moment().toISOString()} | ⬅️  Pong ${service.url} | ${latencyMs}ms | Status ${
+            response.status
+          }`
+        )
+      } else {
+        console.log(
+          `${moment().toISOString()} | ⬅️  Pong failed ${service.url} | Status ${response.status}`
+        )
+      }
+
+      writePing({
+        date,
+        latency: latencyMs,
+        status: response.status,
+        url: service.url,
       })
-    )
+    }
   } catch (err: unknown) {
     console.log(`Cron Error | ${err}`)
   }
